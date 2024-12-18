988ac1e8b34ec7cd3cca6fd6fc779a6f36c3f053
DapotZ <daffahakim378@gmail.com>
2024-12-18 22:53:44 +0700

    Update Dashboard.jsx: Merapikan Nama Folder

--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Content from "../../components/AdminComponent/Dashboard/Content";
 import Navbar from "../../components/AdminComponent/Navbar";
-import Sidebar from "../../components/AdminComponent/SideBar";
+import Sidebar from "../../components/AdminComponent/Webinar/SideBar";
 
 function App() {
   return (
