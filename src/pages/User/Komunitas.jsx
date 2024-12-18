50c05f13bbe7801357462f6ab72b92ddceaa8b3f
DapotZ <daffahakim378@gmail.com>
2024-12-18 22:49:57 +0700

    Update Komunitas.jxs: Merapikan Nama Folder

--- src/pages/User/Komunitas.jsx
+++ src/pages/User/Komunitas.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../../components/UserComponent/Navbar/Navbar";
-import Banner from "../../components/UserComponent/Komunitas/banner";
-import Cards from "../../components/UserComponent/Komunitas/card";
+import Banner from "../../components/UserComponent/Komunitas/Banner";
+import Cards from "../../components/UserComponent/Komunitas/Card";
 import Footer from "../../components/UserComponent/Footer/FooterUser";
 const Komunitas = () => {
   return (
